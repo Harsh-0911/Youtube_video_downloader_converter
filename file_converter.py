7fa1fb1af872e37e49428705052ef4ed8e675c40
Harsh <harshpanchal6822@gmail.com>
2020-12-02 11:40:34 +0530

    Bug fixes

--- file_converter.py
+++ file_converter.py
@@ -1,6 +1,11 @@
 from moviepy.editor import VideoFileClip
 
 def convert_to_mp3(filename):
+    '''
+    Extracts audio from the video file.
+
+    :param filename: name of the video file.
+    '''
     clip = VideoFileClip(filename)
     clip.audio.write_audiofile(filename[:-4] + ".mp3")
     clip.close()
--- video_downloader.py
+++ video_downloader.py
@@ -19,6 +19,12 @@ def input_links():
 
 
 def choose_quality(resolution):
+    '''
+    Take the resolution and returns approprite itag.
+
+    :param resolution: Resolution of video.
+    :return: Relative itag according to specified resolution.
+    '''
     if resolution in ["low", "360", "360p"]:
         itag = 18
     elif resolution in ["medium", "720", "720p", "hd"]:
@@ -35,6 +41,13 @@ def choose_quality(resolution):
 
 
 def download_video(link, quality):
+    '''
+    Downloads video from the youtube of the given quality.
+
+    :param link: URL of the Youtube video
+    :param quality: Quality/Resolution of the video
+    :return: Name of the downloaded video
+    '''
     video_object = pytube.YouTube(link)
     video_itag = choose_quality(quality)
     stream = video_object.streams.get_by_itag(video_itag)
@@ -46,10 +59,22 @@ def download_video(link, quality):
 
 
 def download_videos(urls, quality):
+    '''
+    Downloads multiple videos from the youtube.
+
+    :param urls: List of URLs of youtube videos
+    :param quality: Quality/Resolution of the video
+    '''
     for url in urls:
         download_video(url, quality)
 
 
 def download_playlist(link, quality):
+    '''
+    Downloads the playlist from the youtube.
+
+    :param link: URL of the youtube playlist.
+    :param quality: Quality/Resolution of the video
+    '''
     playlist_object = pytube.Playlist(link)
     download_videos(playlist_object.video_urls, quality)
